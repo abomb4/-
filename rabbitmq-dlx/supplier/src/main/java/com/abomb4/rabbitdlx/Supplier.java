c7dfaeea304a22f4d2e90221a1b30cd51e2e3191
杨睿龙 <yangrl14628@hundsun.com>
2020-02-05 17:04:34 +0800

    publisher confirm

--- rabbitmq-dlx/supplier/src/main/java/com/abomb4/rabbitdlx/Supplier.java
+++ rabbitmq-dlx/supplier/src/main/java/com/abomb4/rabbitdlx/Supplier.java
@@ -3,6 +3,7 @@ package com.abomb4.rabbitdlx;
 import com.abomb4.rabbitdlx.common.constants.MqNames;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -19,14 +20,21 @@ public class Supplier {
     private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(0);
 
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private RabbitTemplate amqpTemplate;
 
     @Scheduled(fixedRate = 5000L)
     public void send() {
         // 发一条消
         final String msg = "[" + ATOMIC_INTEGER.incrementAndGet() + "] The 消息 @@@@";
         log.info("发个消息：[{}]", msg);
-        amqpTemplate.convertAndSend(MqNames.DEMO_EXCHANGE_NAME, MqNames.DEMO_TOPIC, msg);
-        log.info("消息 [{}] 发送成功", msg);
+        final Boolean sent = amqpTemplate.invoke(operations -> {
+            operations.convertAndSend(MqNames.DEMO_EXCHANGE_NAME, MqNames.DEMO_TOPIC, msg);
+            return amqpTemplate.waitForConfirms(3000L);
+        });
+        if (sent) {
+            log.info("消息 [{}] 发送成功", msg);
+        } else {
+            log.error("消息 [{}] 发送失败", msg);
+        }
     }
 }
